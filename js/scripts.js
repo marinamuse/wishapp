b3458271284d28d921e893c398bfc10bbd067bbf
Marina Kim <marinakim@Marinas-MacBook-Pro.local>
2020-12-04 22:51:00 +0500

    add popup modals

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('#monitorButton').click(function() {
+        $("#monitorModal").modal("show")
+    });
+    $('#keyboardButton').click(function() {
+        $("#keyboardModal").modal("show")
+    });
+    $('#controllerButton').click(function() {
+        $("#controllerModal").modal("show")
+    });
+});
