a2941a9472c16369cf556e2835e2ee02b5d5a94a
Marina Kim <marinakim@Marinas-MacBook-Pro.local>
2020-12-05 08:30:21 +0500

    add pages make-wish myfriends myaccount

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-$(function() {
-    $('#monitorButton').click(function() {
-        $("#monitorModal").modal("show")
-    });
-    $('#keyboardButton').click(function() {
-        $("#keyboardModal").modal("show")
-    });
-    $('#controllerButton').click(function() {
-        $("#controllerModal").modal("show")
-    });
+function deleteWish() {
+    $('.monitor').hide();
+    $('#monitorModal').modal("hide")
+}
+
+$('#monitorButton').click(function() {
+    $("#monitorModal").modal("show")
+});
+$('#keyboardButton').click(function() {
+    $("#keyboardModal").modal("show")
+});
+$('#controllerButton').click(function() {
+    $("#controllerModal").modal("show")
 });
